bb1d1a08e4dc650e2b0fef5336468fe5a68df62b
Kishore Prakash <101265520+LIGHTYEAR-K@users.noreply.github.com>
2022-09-06 17:47:27 +0530

    Create Mongodb.js
    Create a Database and Fetch Data from the Database.

---
+++ Mongodb.js
@@ -0,0 +1,26 @@
+const express=require("express");
+const sampledata=require('./sample.json')
+const app=express();
+const {MongoClient}=require('mongodb')
+
+app.get('/',(req,res)=>
+{
+  const url=
+  'mongodb+srv://dbadmin:zvkuty5laL7yLqOQ@demo.xg7xfko.mongodb.net?retryWrites=true&w=majority'
+  const dbName='Sample_datas';
+  
+(async function mongo(){
+  let client;
+  try {
+    client=await MongoClient.connect(url);
+    console.log('connected to the DataBase');
+    const db=client.db(dbName);
+    // const response=await db.collection('sampledata').insertMany(sampledata);   // to Create database
+    // res.json(response);
+    const attack=await db.collection('sampledata').find().toArray;                 // to fetch data from database
+    res.json(attack);
+  } catch (error) {
+    console.log(error.stack)
+  }
+}())
+});
